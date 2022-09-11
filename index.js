f88072f936ca609630153084616a12633bd0a66a
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-09-12 01:44:07 +0300

    feat: add class Filter

--- index.js
+++ index.js
@@ -1,156 +1,109 @@
-"use strict"
-function createLabel(name, text){
-    const label = document.createElement('label');
-    label.setAttribute('for', name);
-    label.textContent = text;
-    return label;
-}
-function createInputControl(id, type, name, min, max, value){
-    const input = document.createElement('input');
-    input.setAttribute('id', id);
-    input.setAttribute('type', type);
-    input.setAttribute('name', name);
-    input.setAttribute('min', min ?? 'none');
-    input.setAttribute('max', max ?? 'none');
-    input.setAttribute('value', value ?? '0');
-    return input;
-
-}
-class Controls{
-    constructor(parent){
-        const controls = document.createElement('div');
-        controls.className = 'controls';
-
-        controls.append(createLabel('blur', 'blur'));
-        const blur = createInputControl('blur', 'range', 'blur', '0', '25','0')
-        blur.onchange = () => {
-            const myImg = document.querySelector('.myImg')
-
-            let styles = myImg.style.filter.split(' ');
-            for(let i = 0; i < styles.length; i++){
-                if (/^blur/.test(styles[i]))
-                    styles[i] = `blur(${blur.value}px)`;
-            }
-            myImg.style.filter = styles.join(' ');
-            console.log(myImg)
-
+const filters = [
+    {
+        blur: {
+            name: 'blur',
+            value: '5px'
         }
-        controls.append(blur);
-
-        controls.append(createLabel('hue', 'hue'));
-        const hue = createInputControl('hue', 'range', 'hue', '0', '200','0')
-        hue.onchange = () => {
-            const myImg = document.querySelector('.myImg');
-            let styles = myImg.style.filter.split(' ');
-            for(let i = 0; i < styles.length; i++){
-                if(/^hue-rotate/.test(styles[i]))
-                    styles[i] = `hue-rotate(${hue.value}deg)`;
-            }
-            myImg.style.filter = styles.join(' ');
+    },
+    {
+        brightness: {
+            name: 'brightness',
+            value: '0.4'
         }
-        controls.append(hue);
-
-        controls.append(createLabel('contrast', 'contrast'));
-
-        const contrast = createInputControl('contrast', 'range', 'contrast', '10', '200','100')
-        contrast.onchange = () => {
-            const myImg = document.querySelector('.myImg')
-            let styles = myImg.style.filter.split(' ');
-            for(let i = 0; i < styles.length; i++){
-                if (/^contrast/.test(styles[i]))
-                    styles[i] = `contrast(${contrast.value}%)`;
-            }
-            myImg.style.filter = styles.join(' ');
-
+    },
+    {
+        contrast: {
+            name: 'contrast',
+            value: '200%'
         }
-        controls.append(contrast);
-
-        const resetBtn = document.createElement('button');
-        resetBtn.className = 'resetBtn';
-        resetBtn.textContent = 'Reset';
-        resetBtn.onclick = () => {
-            let img = document.querySelector('.myImg');
-            let contrast = document.getElementById('contrast');
-            contrast.value = '100';
-            let hue = document.getElementById('hue');
-            hue.value = '0';
-            let blur = document.getElementById('blur');
-            blur.value = '0';
-
-            img.style.filter = 'blur(0px) hue-rotate(0deg) contrast(100%)';
+    },
+    {
+        dropShadow: {
+            name: 'drop-shadow',
+            value: '16px 16px 20px blue'
         }
-        controls.append(resetBtn);
-        parent.append(controls);
+    },
+    {
+        grayscale: {
+            name: 'grayscale',
+            value: '50%'
+        }
+    },
+    {
+        hueRotate: {
+            name: 'hue-rotate',
+            value: '90deg'
+        }
+    },
+    {
+        invert: {
+            name: 'invert',
+            value: '75%'
+        }
+    },
+    {
+        opacity: {
+            name: 'opacity',
+            value: '25%'
+        }
+    },
+    {
+        saturate: {
+            name: 'saturate',
+            value: '30%'
+        }
+    },
+    {
+        sepia: {
+            name: 'sepia',
+            value: '60%'
+        }
+    }
+];
+
+class Filter{
+    constructor(item, parent){
+        let filterId = '';
+        let filterName = '';
+        let filterValue = '';
+        for (let key in item) {
+            filterId = key.toString();
+            filterName = item[key].name;
+            filterValue = item[key].value;
+        }
+        const filterDiv = document.createElement('div');
+        filterDiv.className = filterId;
+        this.node = filterDiv;
+
+        const input = this.createInputControl(filterId, 'range', filterName, '0', '200',filterValue);
+        const label = this.createLabel(filterId, filterId);
+        filterDiv.append(label);
+        filterDiv.append(input);
+        parent.append(filterDiv);
+
+    }
+    createInputControl(id, type, name, min, max, value){
+        const input = document.createElement('input');
+        input.setAttribute('id', id);
+        input.setAttribute('type', type);
+        input.setAttribute('name', name);
+        input.setAttribute('min', min);
+        input.setAttribute('max', max);
+        input.setAttribute('value', value);
+        return input;
+    }
+    createLabel(name, text){
+        const label = document.createElement('label');
+        label.setAttribute('for', name);
+        label.textContent = text;
+        return label;
     }
 }
+function createElements(parent){
 
-function createInput(id, type, name){
-    const input = document.createElement('input');
-    input.setAttribute('id', id);
-    input.setAttribute('type', type);
-    input.setAttribute('name', name);
-    return input;
-
+    filters.forEach((item, i, arr)=>{
+        new Filter(item, parent)
+    })
 }
-
-class File{
-    constructor(parent) {
-        const file = document.createElement('div');
-        file.className = 'file';
-
-        const btns = document.createElement('div');
-        btns.className = 'btns';
-
-        const input = createInput('file', 'file', 'file')
-        input.onchange = () => {
-            let preview = document.querySelector('img');
-            let downloadImg =
-            preview.className = 'myImg'
-
-            let file = document.querySelector('input[type=file]').files[0];
-            let reader = new FileReader();
-
-            reader.onloadend =  () => {
-                let downloadImg = document.querySelector('.downloadImg')
-                preview.src = reader.result;
-                downloadImg.href = preview.src;
-            }
-            if (file) {
-                reader.readAsDataURL(file);
-            } else {
-                preview.src = "https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg";
-            }
-        }
-        btns.append(input);
-
-        const downloadImg = document.createElement('a');
-        downloadImg.setAttribute('href', 'https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg')
-        downloadImg.setAttribute('download', '')
-        downloadImg.textContent = 'download img'
-        downloadImg.className = 'downloadImg'
-        downloadImg.onclick = () => {
-            let styles = document.querySelector('.myImg');
-        }
-
-        btns.append(downloadImg);
-
-        file.append(btns)
-        const img = document.createElement('img')
-        img.src = 'https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg';
-        img.setAttribute('alt', 'broken');
-        img.className = 'myImg';
-        img.style.filter = 'blur(0px) hue-rotate(0deg) contrast(100%)'
-
-        file.append(img);
-        parent.append(file);
-    }
-
-}
-function photoFilter(parent){
-    new Controls(parent);
-    new File(parent);
-}
-
-photoFilter(document.body)
-
+createElements(document.body)
 
