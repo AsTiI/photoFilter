a83a74458739ff22a68c772f58bf65b3646614b5
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-09-12 03:28:15 +0300

    feat: add class Image with download and upload buttons

--- index.js
+++ index.js
@@ -62,7 +62,11 @@ const filters = [
 ];
 
 class Filter{
-    constructor(item, parent){
+    /**
+     *
+     * @param {Object} item
+     */
+    constructor(item){
         let filterId = '';
         let filterName = '';
         let filterValue = '';
@@ -72,16 +76,29 @@ class Filter{
             filterValue = item[key].value;
         }
         const filterDiv = document.createElement('div');
-        filterDiv.className = filterId;
         this.node = filterDiv;
 
+        filterDiv.className = filterId + ' filterItem';
+
         const input = this.createInputControl(filterId, 'range', filterName, '0', '200',filterValue);
+        input.onchange = () => {
+
+        }
         const label = this.createLabel(filterId, filterId);
         filterDiv.append(label);
         filterDiv.append(input);
-        parent.append(filterDiv);
-
     }
+
+    /**
+     *
+     * @param {String} id
+     * @param {String} type
+     * @param {String} name
+     * @param {String} min
+     * @param {String} max
+     * @param {String} value
+     * @returns {HTMLInputElement}
+     */
     createInputControl(id, type, name, min, max, value){
         const input = document.createElement('input');
         input.setAttribute('id', id);
@@ -92,6 +109,13 @@ class Filter{
         input.setAttribute('value', value);
         return input;
     }
+
+    /**
+     *
+     * @param {String} name
+     * @param {String} text
+     * @returns {HTMLLabelElement}
+     */
     createLabel(name, text){
         const label = document.createElement('label');
         label.setAttribute('for', name);
@@ -99,11 +123,107 @@ class Filter{
         return label;
     }
 }
-function createElements(parent){
 
+class Image{
+    constructor(src) {
+        this.src = src;
+        const divImg = document.createElement('div');
+        this.node = divImg;
+
+        divImg.className = 'imgBlock'
+        this.img = this.createImg(this.src);
+        divImg.append(this.img);
+
+        const divBtn = document.createElement('div');
+        divBtn.className = 'imageBtns';
+        divImg.prepend(divBtn);
+
+        const inputUploadImg = this.createInput('file', 'file', 'file');
+        inputUploadImg.onchange = () => {
+            let preview = this.img;
+            let file = inputUploadImg.files[0];
+            console.log(file)
+            let reader = new FileReader(file);
+
+            reader.onloadend = () => {
+                preview.src = reader.result;
+                downloadBtn.href = preview.src;
+            }
+            if (file) {
+                reader.readAsDataURL(file);
+            } else {
+                preview.src = "https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg";
+            }
+        }
+        divBtn.append(inputUploadImg);
+
+        const downloadBtn = this.createBtn('https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg');
+
+        divBtn.append(downloadBtn);
+    }
+
+    /**
+     *
+     * @param {String} id
+     * @param {String} type
+     * @param {String} name
+     * @returns {HTMLInputElement}
+     */
+    createInput(id, type, name){
+        const newInput = document.createElement('input');
+        newInput.setAttribute('id', id);
+        newInput.setAttribute('type', type);
+        newInput.setAttribute('name', name);
+        return newInput
+    }
+
+    /**
+     *
+     * @param {String} src
+     * @returns {HTMLImageElement}
+     */
+    createImg(src){
+        const newImg = document.createElement('img');
+        newImg.src = src;
+        newImg.setAttribute('alt', 'broken');
+        newImg.className = 'myImg';
+        newImg.style.filter = 'blur(0px) hue-rotate(0deg) contrast(100%)';
+
+        return newImg;
+    }
+
+    //-------------------------переделать с ссылки на кнопку---------------------
+    /**
+     *
+     * @param {String} src
+     * @returns {HTMLAnchorElement}
+     */
+    createBtn(src){
+        const newBtn = document.createElement('a');
+        newBtn.setAttribute('href', src)
+        newBtn.setAttribute('download', '')
+        newBtn.textContent = 'download img';
+        newBtn.className = 'downloadImg';
+
+        return newBtn
+    }
+    //----------------------------------------------
+}
+
+/**
+ *
+ * @param {HTMLElement} parent
+ */
+
+function createElements(parent){
+    const div = document.createElement('div');
+    parent.append(div);
+    div.className = 'filters';
     filters.forEach((item, i, arr)=>{
-        new Filter(item, parent)
+
+        div.append(new Filter(item, parent).node);
     })
+    parent.append(new Image('https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg').node);
 }
 createElements(document.body)
 
